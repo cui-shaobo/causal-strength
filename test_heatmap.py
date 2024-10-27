a112a67e1729fa8f643eba854c4ae29e363bafd9
cui-shaobo <shaobo.cui@epfl.ch>
2024-10-27 18:22:35 +0100

    acl for heatmap

--- test_heatmap.py
+++ test_heatmap.py
@@ -13,4 +13,4 @@ s2_cesar = "House burns to ashes."
 
 print("Testing CESAR model:")
 cesar_score = evaluate(s1_cesar, s2_cesar, model_name='CESAR', model_path='huggingfacesc/cesar-bert-large',
-                       plot_heatmap_flag=True, heatmap_path=f'./figures/causal_heatmap.pdf')
+                       plot_heatmap_flag=True, heatmap_path=f'./figures/causal_heatmap.png')
